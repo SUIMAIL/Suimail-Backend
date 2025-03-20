54bc5fad0bdc74e8625d1ee75b2a0a63196058a6
Evangel90 <oladipoevangel90@gmail.com>
2025-03-20 15:13:11 +0100

    ready to test

--- src/routes/mailRoutes.ts
+++ src/routes/mailRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { sendMail, fetchInbox, fetchOutbox } from '../controllers/mailController';
+import { sendMail, fetchInbox, fetchOutbox, fetchMessage } from '../controllers/mailController';
 import inboxMiddleware from '../middlewares/inboxMiddleware';
 import outboxMiddleware from '../middlewares/outboxMiddleware';
 import authMiddleware from '../middlewares/authMiddleware';
@@ -9,5 +9,7 @@ const mailRouter = express.Router();
 mailRouter.post('/sendMail', authMiddleware, sendMail);
 mailRouter.get('/inbox/:to', authMiddleware, inboxMiddleware, fetchInbox);
 mailRouter.get('/outbox/:from', authMiddleware, outboxMiddleware, fetchOutbox);
+mailRouter.get('/inboxMessage/:id', authMiddleware, inboxMiddleware, fetchMessage);
+mailRouter.get('/outboxMessage/:id', authMiddleware, outboxMiddleware, fetchMessage);
 
 export default mailRouter;
