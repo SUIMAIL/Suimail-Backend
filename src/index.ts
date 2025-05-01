cfc3a1131e042b9dd3d2e1ed14bacac51722b692
Evangel90 <oladipoevangel90@gmail.com>
2025-05-01 12:02:06 +0100

    Light Attachments werking, still need make each attachment upload unique and encrypted

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
